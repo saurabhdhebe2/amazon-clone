063fc29b7655a69b82b61a7048cd376bdf77b69e
saurabhdhebe2 <108879055+saurabhdhebe2@users.noreply.github.com>
2022-10-19 23:27:43 +0530

    cjc

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -8,10 +8,6 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
-  secret: process.env.SECRET,
-  pages: {
-    signIn: "/auth/signin",
-  },
 };
 
 export default NextAuth(authOptions);
